b1abe55c37ff0cdfd57a8bc243c4666cf716971a
Ezequiel Gomes <ezequiel7897@gmail.com>
2024-05-04 16:40:39 +0200

    Initial Commit

---
+++ MolkkyCounterPoint/MolkkyCounterPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  MolkkyCounterPointApp.swift
+//  MolkkyCounterPoint
+//
+//  Created by Ezequiel Gomes on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MolkkyCounterPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
